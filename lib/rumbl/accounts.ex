619189779ea2a4b6b98ba3b64a5b58024e2f838c
Tomas Kral <tomas.kral@railsformers.com>
2020-11-14 22:31:13 +0100

    handle sessions and authentification

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -5,6 +5,7 @@ defmodule Rumbl.Accounts do
 
   alias Rumbl.Repo
   alias Rumbl.Accounts.User
+  import Ecto.Query
 
   def list_users do
     Repo.all(User)
@@ -36,6 +37,26 @@ defmodule Rumbl.Accounts do
     %User{}
     |> User.registration_changeset(attrs)
     |> Repo.insert()
+  end
+
+  def get_user_by_email(email) do
+    from(u in User, join: c in assoc(u, :credential), where: c.email == ^email)
+    |> Repo.one()
+    |> Repo.preload(:credential)
+  end
+
+  def authenticate_by_email_and_pass(email, given_pass) do
+    user = get_user_by_email(email)
+
+    cond do
+      user && Comeonin.Pbkdf2.checkpw(given_pass, user.credential.password_hash) ->
+        {:ok, user}
+      user ->
+        {:error, :unauthorized}
+      true ->
+        Comeonin.Pbkdf2.dummy_checkpw()
+        {:error, :not_found}
+    end
   end
 
   alias Rumbl.Accounts.Credential
